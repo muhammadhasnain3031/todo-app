b7a4dbe1177c085b4988ee5490683a0325b74ae9
Muhammad <muhammadhasnain3031@gmail.com>
2026-02-17 11:15:49 -0300

    Initial commit

--- todo-app.js
+++ todo-app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
 const {MongoClient, ObjectId} = require('mongodb');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error(err));
+
 
 const app = express();
 const PORT = 4000;
